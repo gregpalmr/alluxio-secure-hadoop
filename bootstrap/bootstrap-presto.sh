28fa66613c6b0cadd1db089be73cab51d1a4c351
Saiguang Che <saiguang@alluxio.com>
2022-10-31 13:27:33 +0800

    presto

--- bootstrap/bootstrap-presto.sh
+++ bootstrap/bootstrap-presto.sh
@@ -19,28 +19,28 @@ KEYTAB_DIR=/etc/security/keytabs
 FQDN=hadoop.com
 PRESTO_SERVER_FQDN=presto-server.docker.com
 
-if [ -d ${KEYTAB_DIR} ] && [ -f ${KEYTAB_DIR}/presto.service.keytab ]; then
-  echo "- File ${KEYTAB_DIR}/presto.service.keytab exists, skipping create kerberos principals step"
-else 
-  echo "- Creating kerberos principals for presto.service"
+# if [ -d ${KEYTAB_DIR} ] && [ -f ${KEYTAB_DIR}/presto.service.keytab ]; then
+#   echo "- File ${KEYTAB_DIR}/presto.service.keytab exists, skipping create kerberos principals step"
+# else 
+#   echo "- Creating kerberos principals for presto.service"
 
-  # save cwd and cd to $KEYTAB_DIR
-  pushd ${KEYTAB_DIR}
+#   # save cwd and cd to $KEYTAB_DIR
+#   pushd ${KEYTAB_DIR}
 
-  kadmin \
-  -p ${KERBEROS_ADMIN} -w ${KERBEROS_ADMIN_PASSWORD} \
-  -q "addprinc -randkey -maxrenewlife 7d +allow_renewable presto/${PRESTO_SERVER_FQDN}@${KRB_REALM}"
+#   kadmin \
+#   -p ${KERBEROS_ADMIN} -w ${KERBEROS_ADMIN_PASSWORD} \
+#   -q "addprinc -randkey -maxrenewlife 7d +allow_renewable presto/${PRESTO_SERVER_FQDN}@${KRB_REALM}"
 
-  kadmin -p ${KERBEROS_ADMIN} -w ${KERBEROS_ADMIN_PASSWORD} \
-  -q "xst -k presto.service.keytab presto/${HADOOP_NAMENODE_FQDN}"
+#   kadmin -p ${KERBEROS_ADMIN} -w ${KERBEROS_ADMIN_PASSWORD} \
+#   -q "xst -k presto.service.keytab presto/${HADOOP_NAMENODE_FQDN}"
 
-  chmod 400 ${KEYTAB_DIR}/presto.service.keytab
+#   chmod 400 ${KEYTAB_DIR}/presto.service.keytab
 
-  # cd back to previously cwd
-  popd
-fi
+#   # cd back to previously cwd
+#   popd
+# fi
 
 
-# start presto server
-#
-/bin/sh -c ./bin/launcher run
+# # start presto server
+# #
+# /bin/sh -c ./bin/launcher run
